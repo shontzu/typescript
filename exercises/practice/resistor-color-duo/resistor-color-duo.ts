22ce689c5fb98cc0cd5679f19c66990a0fd500d5
Keoy <shon.tzu@regentmarkets.com>
2023-01-13 15:42:10 +0800

    q3: resistor-color-duo

--- exercises/practice/resistor-color-duo/resistor-color-duo.ts
+++ exercises/practice/resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,22 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+// PSEUDOCODE:
+// loop through array of colors
+// get the indexOf each color by comparing with ColorArr
+// concat the two indexOf.toString()
+// parseInt and return
+
+export function decodedValue([col1, col2]: any): number {
+  const ColorArr: string[] = [
+    `black`,
+    `brown`,
+    `red`,
+    `orange`,
+    `yellow`,
+    `green`,
+    `blue`,
+    `violet`,
+    `grey`,
+    `white`,
+  ]
+
+  return parseInt(ColorArr.indexOf(col1).toString() + ColorArr.indexOf(col2).toString())
 }
